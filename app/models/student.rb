0c8ef2654cc5d6ba10417303ea47a9d8d436c86f
yungpanko <jrjohns@mit.edu>
2017-06-09 17:40:16 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+    if self.active == true
+      self.active = false
+    elsif self.active == false
+      self.active = true
+    end
+    self.save
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+students = Student.create([{ first_name: 'Jared', last_name: 'Johnson' }, { first_name: 'Larson', last_name: 'Laidlaw' }])
